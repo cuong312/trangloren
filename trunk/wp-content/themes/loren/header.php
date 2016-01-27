2aca1e63e52dd789cb9f9668f205275396614b78
cuong.bui <cuong.bui@savvycom.vn>
2016-01-27 15:34:47 +0700

    update meta scale in header

--- trunk/wp-content/themes/loren/header.php
+++ trunk/wp-content/themes/loren/header.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!-- Google Fonts -->
 <link href='http://fonts.googleapis.com/css?family=Roboto:400,900italic,700italic,900,700,500italic,500,400italic,300italic,300,100italic,100|Open+Sans:400,300,400italic,300italic,600,600italic,700italic,700,800|Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,600italic,700' rel='stylesheet' type='text/css'>
 
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
-<link rel="stylesheet" type="text/css" href="css/ie.css" />
-<script type="text/javascript" language="javascript" src="js/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 
 <?php wp_head(); ?>
